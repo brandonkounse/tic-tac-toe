40d03dd6d965818b1ae4fcd65d20915e4e90a9b5
Brandon <kounse@gmail.com>
2022-12-10 16:18:30 -0600

    Refactored main tic_tac_toe.rb file, and refactored players.rb class file.

--- players.rb
+++ players.rb
@@ -2,16 +2,37 @@
 
 # create players for game session
 class Players
-  attr_accessor :name, :game_piece
+  class << self
+    attr_accessor :pieces
+  end
 
-  def initialize
-    puts 'Enter player name: '
+  attr_accessor :name, :game_piece, :player_number
+
+  def initialize(player_x)
+    @player_number = player_x
+  end
+
+  def player_info
+    puts "Enter player #{player_number} name: "
     self.name = gets.chomp
   end
 
   def x_or_o
-    puts 'Please pick X or O for first player'
+    puts "Please pick X or O for #{player_number} player"
     self.game_piece = gets.chomp.upcase
     x_or_o unless game_piece == 'X' || game_piece == 'O'
+  end
+
+  def update_player_pieces
+    if Players.pieces.nil?
+      x_or_o
+      Players.pieces = [game_piece]
+    elsif Players.pieces.include?('X')
+      self.game_piece = 'O'
+      Players.pieces += [game_piece]
+    else
+      self.game_piece = 'X'
+      Players.pieces += [game_piece]
+    end
   end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
-require game_board.rb
-require players.rb
+$LOAD_PATH << '.'
+require 'game_board'
+require 'players'
 
-p1 = Players.new
-p1.x_or_o
-puts p1.game_piece
+p1 = Players.new(1)
+p2 = Players.new(2)
+p1.player_info
+p2.player_info
+p1.update_player_pieces
+p2.update_player_pieces
+puts "player pieces are #{Players.pieces}"
+puts "player 1 is #{p1.game_piece} and player 2 is #{p2.game_piece}"
