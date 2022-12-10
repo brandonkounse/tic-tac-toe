35f0deee6772e346c2650dc3871e6594f4fa1c2e
Brandon <kounse@gmail.com>
2022-12-09 20:12:43 -0600

    Rewrote players class and deleted other code.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
-class Player
-  @@players = []
+# create players for game session
+class Players
+  attr_accessor :name, :game_piece
 
-  def initialize(player, _x_or_o)
-    @@players += player
+  def initialize
+    puts 'Enter player name: '
+    self.name = gets.chomp
+  end
+
+  def x_or_o
+    puts 'Please pick X or O for first player'
+    self.game_piece = gets.chomp.upcase
+    x_or_o unless game_piece == 'X' || game_piece == 'O'
   end
 end
+
+p1 = Players.new
+p1.x_or_o
+puts p1.game_piece
