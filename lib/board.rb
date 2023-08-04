c74e85632d0dda76eb063a83ee3dc16274c3e880
Brandon Kounse <kounse@gmail.com>
2023-08-03 21:28:42 -0500

    changed name of #draw_board to #draw

--- lib/board.rb
+++ lib/board.rb
@@ -8,7 +8,7 @@ class Board
     @winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   end
 
-  def draw_board
+  def draw
     puts "\n #{@squares[0..2].join(' | ')}"
     puts "-----------\n"
     puts " #{@squares[3..5].join(' | ')}"
