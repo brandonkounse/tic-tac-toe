92b3e67de35583e81de2e88f53fb49e28513fbcc
Brandon Kounse <kounse@gmail.com>
2023-08-07 16:34:43 -0500

    refactored main file re-written classes

--- main.rb
+++ main.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
 require_relative 'lib/player'
-require_relative 'lib/game_board'
+require_relative 'lib/board'
 require_relative 'lib/tic_tac_toe'
 
-p1 = Player.new('Player 1', 'X')
-p2 = Player.new('Player 2', 'O')
-board = GameBoard.new
-game = TicTacToe.new
-
-while game.game_over == false
-  game.list_players(p1, p2)
-  game.game_loop(p1, board, game)
-  break unless game.game_over == false
-
-  game.list_players(p1, p2)
-  game.game_loop(p2, board, game)
-end
+player_one = Player.new('Player 1', 'X')
+player_two = Player.new('Player 2', 'O')
+game = TicTacToe.new(player_one, player_two)
+game.play
